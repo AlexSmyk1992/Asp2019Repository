cdb860e6417a4a4ea000974999fc8ecd604d1c45
AlexSmyk <alexoya2@mail.ru>
2019-02-26 22:38:10 +0300

    Edit/Delete/Add with Models

--- BookingStore/BookingStore/Controllers/HomeController.cs
+++ BookingStore/BookingStore/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using BookingStore.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,7 +14,108 @@ namespace BookingStore.Controllers
 
         public ActionResult Index()
         {
+
+            return View(db.Books.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Book b = db.Books.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+
+        [HttpPost, ActionName("Delete")]
+
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book b = db.Books.Find(id);
+            if(b == null)
+            {
+                return HttpNotFound();
+                
+            }
+            db.Books.Remove(b);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditBook(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Book book = db.Books.Find(id);
+            if (book != null)
+            {
+                return View(book);
+            }
+            return HttpNotFound();
+        }
+        [HttpPost]
+        public ActionResult EditBook(Book book)
+        {
+            db.Entry(book).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Unsafe->
+        //public ActionResult Delete(int id)
+        //{
+        //    Book b = db.Books.Find(id);
+        //    if (b != null)
+        //    {
+        //        db.Books.Remove(b);
+        //        db.SaveChanges();
+        //    }
+
+        //    //Book b = new Book { Id = id };
+        //    //db.Entry(b).State = EntityState.Deleted;
+        //    //db.SaveChanges();
+        //    //return RedirectToAction("Index");
+        //}
+
+        public ActionResult GetBook(int id)
+        {
+            Book b = db.Books.Find(id);
+            if (b == null)
+                return HttpNotFound();
+            return View(b);
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            return View(new Purchase { BookId = id, Person = "Hello" });
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return "Thanks," + purchase.Person + ", for purchase";
         }
 
         public ActionResult About()
--- BookingStore/BookingStore/Models/Purchase.cs
+++ BookingStore/BookingStore/Models/Purchase.cs
@@ -7,7 +7,11 @@ namespace BookingStore.Models
 {
     public class Purchase
     {
-
+        public int PurchaseId { get; set; }
+        public string Person { get; set; }
+        public string Address { get; set; }
+        public int BookId { get; set; }
+        public DateTime Date { get; set; }
     }
 
 }
