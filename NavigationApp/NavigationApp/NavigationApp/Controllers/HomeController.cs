5f41a238040d94f02393cf60008fc000d02f5759
AlexSmyk <alexoya2@mail.ru>
2019-03-01 23:19:11 +0300

    add 28 lesson

--- NavigationApp/NavigationApp/NavigationApp/Controllers/HomeController.cs
+++ NavigationApp/NavigationApp/NavigationApp/Controllers/HomeController.cs
@@ -1,18 +1,36 @@
-﻿using System;
+﻿using NavigationApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace NavigationApp.Controllers
 {
     public class HomeController : Controller
     {
+        SoccerContext db = new SoccerContext();
         public ActionResult Index()
         {
-            return View();
+            var players = db.Players.Include(p => p.Team);
+            return View(players.ToList());
         }
 
+        [HttpPost]
+        public ActionResult Create()
+        {
+            SelectList teams = new SelectList(db.Teams, "Id", "Name");
+            ViewBag.Teams = teams;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            db.Players.Add(player);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
